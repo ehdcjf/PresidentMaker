98cb6e030c58956d5a603becbb0ab976110a4aca
ehdcjf <kdch0823@naver.com>
2021-12-03 12:29:34 +0900

    git ignore 수정

--- front/components/home/Footer/index.jsx
+++ front/components/home/Footer/index.jsx
@@ -11,30 +11,35 @@ const StyledFooter = styled.section`
   text-align: center;
   position: relative;
 
+
+  .flexbox{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+
   *{
     border: none;
   }
 
-  .silhouette_container{
-    margin:0 auto;
-    width: 100vw;
-    height: 100vh;
-    top:100px;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
+
   
 
   .silhouette{
-    background-size: contain;
+    background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     width: 40%;
-    height: 40%;
+    height: 100%;
   }
 
-  
+  .slhouette_second{
+    font-size: 5vh;
+    padding: 1vw;
+  }
 
   
 
@@ -60,55 +65,23 @@ const Footer = () => {
     if (onScreen) setReveal(onScreen);
   }, [onScreen]);
 
-  useEffect(() => {
-    if (reveal) {
-      const split = new SplitText("#location-text", {
-        type: "lines",
-        linesClass: "lineChildren",
-      });
-      const splitParent = new SplitText("#location-text", {
-        type: "lines",
-        linesClass: "lineParent",
-      });
-
-      gsap.fromTo(
-        split.lines,
-        { y: 200 },
-        {
-          duration: 1,
-          y: 0,
-          opacity: 1,
-          stagger: 0.1,
-          ease: "power2",
-        }
-      );
-    }
-  }, [reveal]);
+  
 
   return (
     <StyledFooter data-scroll-section>
       
-      <div className='silhouette_container'>
+
           <SectionHeader className='grid_5' title="Who is Next" />
- 
+                  <div className='slhouette_second'>
+              당신의 후보에게 투표하세요.
+        </div>
+          <div className='flexbox'>
           <div className='silhouette posi2'  style={{ backgroundImage: `url(/투명재명.png)` }}></div>
           <div className='silhouette posi1'  style={{ backgroundImage: `url(/투명석열.png)` }}></div>
           <div className='silhouette posi3'  style={{ backgroundImage: `url(/투명낙연.png)` }}></div>
           <div className='silhouette posi4'  style={{ backgroundImage: `url(/투명준표.png)` }}></div>
+          </div>
 
-        <div className='slhouette_second'>
-              당신의 후보에게 투표하세요.
-        </div>
-
-
-      </div>
-      <h1
-        className={cn("location", { "is-reveal": reveal })}
-        id="location-text"
-        ref={ref}
-      >
-        
-      </h1>
     </StyledFooter>
   );
 };
